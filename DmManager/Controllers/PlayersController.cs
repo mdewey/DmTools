fcc24a9b62388979c833ae3c37c282d7785130ed
Mark Dewey <mark.t.dewey@gmail.com>
2019-10-26 17:10:37 -0400

    added player API starting point

---
+++ DmManager/Controllers/PlayersController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DmManager;
+using DmManager.Models;
+
+namespace DmManager.Controllers
+{
+  [Route("api/game/{gameId}/[controller]")]
+  [ApiController]
+  public class PlayersController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public PlayersController(DatabaseContext context)
+    {
+      _context = context;
+    }
+
+    // GET: api/Player
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Player>>> GetPlayers(int gameId)
+    {
+      return await _context.Players.Where(w => w.GameId == gameId).ToListAsync();
+    }
+
+    // // GET: api/Player/5
+    // [HttpGet("{id}")]
+    // public async Task<ActionResult<Player>> GetPlayer(int id)
+    // {
+    //   var player = await _context.Players.FindAsync(id);
+
+    //   if (player == null)
+    //   {
+    //     return NotFound();
+    //   }
+
+    //   return player;
+    // }
+
+    // // PUT: api/Player/5
+    // // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // // more details see https://aka.ms/RazorPagesCRUD.
+    // [HttpPut("{id}")]
+    // public async Task<IActionResult> PutPlayer(int id, Player player)
+    // {
+    //   if (id != player.Id)
+    //   {
+    //     return BadRequest();
+    //   }
+
+    //   _context.Entry(player).State = EntityState.Modified;
+
+    //   try
+    //   {
+    //     await _context.SaveChangesAsync();
+    //   }
+    //   catch (DbUpdateConcurrencyException)
+    //   {
+    //     if (!PlayerExists(id))
+    //     {
+    //       return NotFound();
+    //     }
+    //     else
+    //     {
+    //       throw;
+    //     }
+    //   }
+
+    //   return NoContent();
+    // }
+
+    // // POST: api/Player
+    // // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // // more details see https://aka.ms/RazorPagesCRUD.
+    // [HttpPost]
+    // public async Task<ActionResult<Player>> PostPlayer(Player player)
+    // {
+    //   _context.Players.Add(player);
+    //   await _context.SaveChangesAsync();
+
+    //   return CreatedAtAction("GetPlayer", new { id = player.Id }, player);
+    // }
+
+    // // DELETE: api/Player/5
+    // [HttpDelete("{id}")]
+    // public async Task<ActionResult<Player>> DeletePlayer(int id)
+    // {
+    //   var player = await _context.Players.FindAsync(id);
+    //   if (player == null)
+    //   {
+    //     return NotFound();
+    //   }
+
+    //   _context.Players.Remove(player);
+    //   await _context.SaveChangesAsync();
+
+    //   return player;
+    // }
+
+    private bool PlayerExists(int id)
+    {
+      return _context.Players.Any(e => e.Id == id);
+    }
+  }
+}
